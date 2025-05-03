c67387c3319b67039091de2dd20815d2a9c0f40a
MiggyAbadonraba <MiggyDonLise@gmail.com>
2025-05-03 17:47:52 +0300

    Add main and main_tets files

---
+++ scripts/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func VolumeOfSphere(radius float64) float64 {
+	return math.Pi * (4.0 / 3.0) * math.Pi * math.Pow(radius, 3)
+}
+
+func main() {
+	sphereVolume := VolumeOfSphere(5.0)
+	fmt.Printf("Объём шара: %v m^3", sphereVolume)
+}
---
+++ scripts/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math"
+	"testing"
+)
+
+func TestVolumeOfSphere(t *testing.T) {
+	tests := []struct {
+		radius float64
+		want   float64
+	}{
+		{1.0, (4.0 / 3.0) * math.Pi},
+		{2.0, (32.0 / 3.0) * math.Pi},
+		{0.0, 0.0},
+	}
+
+	for _, tt := range tests {
+		got := VolumeOfSphere(tt.radius)
+		if got != tt.want {
+			t.Errorf("VolumeOfSphere(%v) = %v, want %v", tt.radius, got, tt.want)
+		}
+	}
+}
